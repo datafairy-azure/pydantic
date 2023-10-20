4d781166c72c3836bffae6ba3cc6daf04000f2f6
Martha Lasia <marthalasia@gmail.com>
2023-10-20 11:02:54 +0200

    added json example

---
+++ examples/data-model-json.py
@@ -0,0 +1,89 @@
+"""Example from https://github.com/ArjanCodes/2021-pydantic/blob/main/example.py"""
+import json
+import pydantic
+from typing import List, Optional
+
+
+class ISBNMissingError(Exception):
+    """Custom error that is raised when both ISBN10 and ISBN13 are missing."""
+
+    def __init__(self, title: str, message: str) -> None:
+        self.title = title
+        self.message = message
+        super().__init__(message)
+
+
+class ISBN10FormatError(Exception):
+    """Custom error that is raised when ISBN10 doesn't have the right format."""
+
+    def __init__(self, value: str, message: str) -> None:
+        self.value = value
+        self.message = message
+        super().__init__(message)
+
+
+class Author(pydantic.BaseModel):
+    name: str
+    verified: bool = False
+
+
+class Book(pydantic.BaseModel):
+    """Represents a book with that you can read from a JSON file."""
+
+    title: str
+    author: str
+    publisher: str
+    price: float
+    isbn_10: Optional[str]
+    isbn_13: Optional[str]
+    subtitle: Optional[str] = None
+    author2: Optional[Author] = None
+
+    @pydantic.model_validator(mode="before")
+    @classmethod
+    def check_isbn_10_or_13(cls, values):
+        """Make sure there is either an isbn_10 or isbn_13 value defined"""
+        if "isbn_10" not in values and "isbn_13" not in values:
+            raise ISBNMissingError(
+                title=values["title"],
+                message="Document should have either an ISBN10 or ISBN13",
+            )
+        return values
+
+    @pydantic.field_validator("isbn_10")
+    @classmethod
+    def isbn_10_valid(cls, value) -> None:
+        """Validator to check whether ISBN10 is valid"""
+        chars = [c for c in value if c in "0123456789Xx"]
+        if len(chars) != 10:
+            raise ISBN10FormatError(value=value, message="ISBN10 should be 10 digits.")
+
+        def char_to_int(char: str) -> int:
+            if char in "Xx":
+                return 10
+            return int(char)
+
+        if sum((10 - i) * char_to_int(x) for i, x in enumerate(chars)) % 11 != 0:
+            raise ISBN10FormatError(
+                value=value, message="ISBN10 digit sum should be divisible by 11."
+            )
+        return value
+
+    class Config:
+        """Pydantic config class"""
+
+        str_to_lower = True
+
+
+def main() -> None:
+    """Main function."""
+
+    with open("./data/books.json") as file:
+        data = json.load(file)
+        books: List[Book] = [Book(**item) for item in data]
+
+        print(books[0])
+
+
+if __name__ == "__main__":
+    main()
